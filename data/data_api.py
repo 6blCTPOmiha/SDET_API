900276ecc95b6c417ebb871fb5ceb497036ba4f2
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-07 17:57:25 +0400

    Файл со ссылками и локаторами В дальнейшем распределю корректно

---
+++ data/data_api.py
@@ -0,0 +1,10 @@
+class BaseApiLocators:
+
+    URL = 'http://localhost:8003/api/_/docs/swagger/index.html#/'
+    URL_CREATE = 'http://localhost:8003/api/create'
+    URL_DELETE = 'http://localhost:8003/api/delete'
+    URL_GET = 'http://localhost:8003/api/get'
+    URL_GET_ALL = 'http://localhost:8003/api/getAll'
+    URL_PATCH = 'http://localhost:8003/api/patch'
+
+    TOKEN = '<PASSWORD>'
