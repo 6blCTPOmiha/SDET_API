470746c690ad3ed441d599b1fb2c7faa198711f7
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-08 00:54:52 +0400

    Конвертер(Десериализатор/мб.Серриализатор) Надо бы вынести сюда Серриализатор

---
+++ helpers/convert_helper.py
@@ -0,0 +1,37 @@
+import json
+from models.entity_model import EntityModelResponse
+
+
+
+class ConvertHelper:
+
+    @staticmethod
+    def deserialize_response(response_text: str) -> EntityModelResponse:
+        """Десериализация API в Pydantic"""
+        try:
+            json_data = json.loads(response_text)
+            entity = EntityModelResponse(**json_data)
+            return entity
+
+        except json.JSONDecodeError as e:
+            print(f"Ошибка парсинга JSON: {e}")
+            raise
+        except Exception as e:
+            print(f"Ошибка валидации: {e}")
+            raise
+
+    @staticmethod
+    def deserialize_responses(response_text: str) -> list[EntityModelResponse]:
+        """API в список Pydantic моделей"""
+        try:
+            json_data = json.loads(response_text)
+            entities_data = json_data["entity"]
+            entities = [EntityModelResponse(**entity_data) for entity_data in entities_data]
+            return entities
+
+        except json.JSONDecodeError as e:
+            print(f"Ошибка парсинга JSON: {e}")
+            raise
+        except Exception as e:
+            print(f"Ошибка валидации: {e}")
+            raise
