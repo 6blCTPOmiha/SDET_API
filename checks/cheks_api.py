26bea7d4c0624f7364c4df37b0ebb3d22f731284
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-08 00:57:06 +0400

    Файл в котором будет набор проверок для всех 5 тестов В идеале распределить проверки

---
+++ checks/cheks_api.py
@@ -0,0 +1,20 @@
+import allure
+
+
+class ChecksApi:
+    def __init__(self, driver):
+
+        super().__init__(driver)
+
+
+
+
+    @staticmethod
+    @allure.step('Проверка. Количество сущностей > 2')
+    def check_count_of_entities(ids):
+        assert len(ids) > 2, "Количество сущностей меньше или равно 2"
+
+    @staticmethod
+    @allure.step('Проверка. id Созданного объекта вернулся')
+    def check_entity_created(obj_id):
+        assert obj_id > 0, "Объект не создался"
