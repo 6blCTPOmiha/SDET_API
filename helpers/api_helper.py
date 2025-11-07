c5c86df04640e954f0c2446f2b0f4816f6601ad2
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-08 01:01:54 +0400

    Сделал 3 основных метода для 3 пунктов задания

--- helpers/api_helper.py
+++ helpers/api_helper.py
@@ -1,12 +1,15 @@
 import allure
 from api.requests.base_requests_api import BaseApi
-from data.data_api import BaseApiLocators
+from data.data_api import URL, URL_CREATE, URL_DELETE, URL_GET, URL_GET_ALL, TOKEN
+from data.data_objs import EMREQ0
+from helpers.convert_helper import ConvertHelper
+
 
 
 class ApiHelper(BaseApi):
     def __init__(self):
         super().__init__()
-        self.token = BaseApiLocators.TOKEN
+        self.token = TOKEN
 
     @allure.step("Проверка кода ответа")
     def msg_response_code(self, response, path, obj='obj'):
@@ -30,23 +33,49 @@ class ApiHelper(BaseApi):
             json_response = response.json()
             print(path, response, json_response)
 
+
+    def create_model_request(self):
+        url = URL_CREATE
+        payload = EMREQ0.model_dump()
+        response = self.request_post(url, json_req=payload)
+        response.raise_for_status()
+        return response.json()
+
+
+    def get_msg_response_by_id(self, element_id: int):
+        url_id = f'{URL_GET}/{element_id}'
+        response_text = self.request_get(url_id).text
+        response_ch = ConvertHelper.deserialize_response(response_text)
+        return response_ch.title
+
+
+    def get_ids_msg_response(self):
+        url = URL_GET_ALL
+        response_text = self.request_get(url).text
+        response_chs = ConvertHelper.deserialize_responses(response_text)
+        ids = []
+        for ch in response_chs:
+            ids.append(ch.id)
+        return ids
+
+
     @allure.step("Создание каталога")
     def dir_create(self, path):
-        req_path = f'{BaseApiLocators.URL}?path={path}'
+        req_path = f'{URL}?path={path}'
         response = self.request_put(req_path)
         self.msg_response_code(response, path=path)
         assert response.status_code == 201, "Directory not created!"
 
     @allure.step("Удаление каталога")
     def dir_delete(self, path):
-        req_path = f'{BaseApiLocators.URL}?path={path}'
+        req_path = f'{URL}?path={path}'
         response = self.request_put(req_path)
         self.msg_response_code(response, path=path)
         assert response.status_code == 201, "Directory not created!"
 
     @allure.step("Сканирование каталога")
     def dir_create(self, path):
-        req_path = f'{BaseApiLocators.URL}?path={path}'
+        req_path = f'{URL}?path={path}'
         response = self.request_put(req_path)
         self.msg_response_code(response, path=path)
         assert response.status_code == 201, "Directory not created!"
