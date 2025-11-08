7629bd7ade4ea757b60f05d05d31806bf0dcfb19
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-09 00:05:30 +0400

    Добавлено и изменено несколько проверок - Добавлена проверка на увеличение количества сущностей при создании новой - Изменено название переменной в проверке на удаление - Шаг аллюра скорректирован в соответствии с более стабильной проверкой - Добавлена простая проверка на использование PATCH

--- checks/cheks_api.py
+++ checks/cheks_api.py
@@ -16,11 +16,21 @@ class ChecksApi:
         assert obj_id > 0, "Объект не создался"
 
     @staticmethod
-    @allure.step('Проверка. Объект удалён')
-    def check_entity_deleted(obj_id):
-        assert obj_id == 204, "Объект не удалился"
+    @allure.step('Проверка. Количество сущностей возросло на 1')
+    def check_new_1_entity_created(old, new):
+        assert old + 1 == new, "Количество сущностей не изменилось или изменилось не корректно"
 
     @staticmethod
-    @allure.step('Проверка. Заголовок 3 сущности верный')
+    @allure.step('Проверка. Объект удалён')
+    def check_entity_deleted(resp_num):
+        assert resp_num == 204, "Объект не удалился"
+
+    @staticmethod
+    @allure.step('Проверка. Заголовок 2 сущности верный')
     def check_title_is_right(title):
         assert title == "Заголовок сущности", "Заголовок не верный"
+
+    @staticmethod
+    @allure.step('Проверка. Данные объекта обновлены')
+    def check_entity_patched(resp_num):
+        assert resp_num == 204, "Данные объекта не обновлены"
