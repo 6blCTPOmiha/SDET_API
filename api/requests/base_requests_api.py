9208637e209b4fc465174219ac6deb4757213684
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 13:04:37 +0400

    Убрано лишнее

--- api/requests/base_requests_api.py
+++ api/requests/base_requests_api.py
@@ -1,16 +1,9 @@
-import json
-
-import allure
 import requests
 from requests import Response
 
 
 class BaseApi:
-    def __init__(self):
-        self.token = None
-        self.headers = None
-        self.user_id = None
-        self.body = None
+
 
     @staticmethod
     def request_get(url: str) -> Response:
@@ -20,11 +13,9 @@ class BaseApi:
     def request_post(url: str, json_req, **kwargs: dict) -> Response:
         return requests.post(url, json=json_req, timeout=10, **kwargs)
 
-    def request_put(self, url: str, **kwargs: dict) -> Response:
-        return requests.put(url, headers=self.headers, **kwargs)
-
-    def request_delete(self, url: str, **kwargs: dict) -> Response:
-        return requests.delete(url, headers=self.headers, **kwargs)
+    @staticmethod
+    def request_delete(url: str, **kwargs: dict) -> Response:
+        return requests.delete(url, **kwargs)
 
     @staticmethod
     def request_patch(url: str, json_req, **kwargs: dict) -> Response:
