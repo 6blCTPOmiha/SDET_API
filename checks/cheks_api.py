6b25ec7e335ebd64ee1d611c8d90b98820e48db5
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 12:32:56 +0400

    Добавлены проверки на статус коды и скорректированы другие проверки

--- checks/cheks_api.py
+++ checks/cheks_api.py
@@ -6,14 +6,14 @@ class ChecksApi:
         super().__init__(driver)
 
     @staticmethod
-    @allure.step('Проверка. Количество сущностей > 2')
-    def check_count_of_entities(ids):
-        assert len(ids) > 2, "Количество сущностей меньше или равно 2"
+    @allure.step('Проверка. id Созданного объекта вернулся')
+    def check_create_resp_num(code):
+        assert code == 200, "Объект не создался"
 
     @staticmethod
     @allure.step('Проверка. id Созданного объекта вернулся')
-    def check_entity_created(obj_id):
-        assert obj_id > 0, "Объект не создался"
+    def check_create_resp_id(resp_id):
+        assert resp_id != 0, "Объект не создался"
 
     @staticmethod
     @allure.step('Проверка. Количество сущностей возросло на 1')
@@ -22,15 +22,35 @@ class ChecksApi:
 
     @staticmethod
     @allure.step('Проверка. Объект удалён')
-    def check_entity_deleted(resp_num):
-        assert resp_num == 204, "Объект не удалился"
+    def check_entity_deleted(code):
+        assert code == 204, "Объект не удалился"
 
     @staticmethod
-    @allure.step('Проверка. Заголовок 2 сущности верный')
+    @allure.step('Проверка. Заголовок 2 сущности присутствует')
     def check_title_is_right(title):
-        assert title == "Заголовок сущности", "Заголовок не верный"
+        assert title != "", "Заголовок отсутствует"
+
+    @staticmethod
+    @allure.step('Проверка. Количество сущностей > 2')
+    def check_count_of_entities(ids):
+        assert len(ids) > 0, "Количество сущностей меньше или равно 2"
+
+    @staticmethod
+    @allure.step('Проверка. id Созданного объекта вернулся')
+    def check_get_all_resp_num(code):
+        assert code == 200, "Объект не создался"
+
+    @staticmethod
+    @allure.step('Проверка. Код ответа положительный')
+    def check_get_resp_num(code):
+        assert code == 200, "Объект не выдался"
 
     @staticmethod
     @allure.step('Проверка. Данные объекта обновлены')
-    def check_entity_patched(resp_num):
-        assert resp_num == 204, "Данные объекта не обновлены"
+    def check_entity_patched(code):
+        assert code == 204, "Данные объекта не обновлены"
+
+    @staticmethod
+    @allure.step('Проверка. БД очищена')
+    def check_db_cleared(code):
+        assert code == "OK", "Больше нечего удалять"
