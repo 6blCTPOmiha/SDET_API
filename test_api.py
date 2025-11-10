136c848dfcb7377eb96e6de3fba1a90854babbe7
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 23:20:43 +0400

    Метки приведены в порядок

--- test_api.py
+++ test_api.py
@@ -14,13 +14,13 @@ class TestCustomAPI:
         AssertionsApi.check_create_resp_id(resp.text)
 
     @allure.title('Проверка удаления последней сущности')
-    @pytest.mark.delete_by_id
+    @pytest.mark.delete
     def test_delete_last(self, ent_api):
         resp = ent_api.delete_last_entity()
         AssertionsApi.assert_status_code(204, resp.status_code)
 
     @allure.title('Проверка получения последней сущности')
-    @pytest.mark.get_by_id
+    @pytest.mark.get
     def test_get_last(self, ent_api):
         status_code, title = ent_api.get_msg_response_by_last_id()
         AssertionsApi.assert_status_code(200, status_code)
@@ -33,7 +33,7 @@ class TestCustomAPI:
         AssertionsApi.assert_status_code(200, status_code)
 
     @allure.title('Проверка изменения параметров последней сущности')
-    @pytest.mark.patch_by_id
+    @pytest.mark.patch
     def test_patch(self, ent_api):
         status_code = ent_api.patch_by_last_id(EMREQ0)
         AssertionsApi.assert_status_code(204, status_code)
