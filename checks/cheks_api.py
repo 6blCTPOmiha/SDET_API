2eeed57d200272719c25acc3a16ea0f1d619ba35
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 13:05:54 +0400

    Проверки приведены в порядок

--- checks/cheks_api.py
+++ checks/cheks_api.py
@@ -6,7 +6,7 @@ class ChecksApi:
         super().__init__(driver)
 
     @staticmethod
-    @allure.step('Проверка. id Созданного объекта вернулся')
+    @allure.step('Проверка. Код ответа положительный')
     def check_create_resp_num(code):
         assert code == 200, "Объект не создался"
 
@@ -16,27 +16,22 @@ class ChecksApi:
         assert resp_id != 0, "Объект не создался"
 
     @staticmethod
-    @allure.step('Проверка. Количество сущностей возросло на 1')
-    def check_new_1_entity_created(old, new):
-        assert old + 1 == new, "Количество сущностей не изменилось или изменилось не корректно"
-
-    @staticmethod
-    @allure.step('Проверка. Объект удалён')
+    @allure.step('Проверка. Объект удалён. Код ответа положительный')
     def check_entity_deleted(code):
         assert code == 204, "Объект не удалился"
 
     @staticmethod
-    @allure.step('Проверка. Заголовок 2 сущности присутствует')
+    @allure.step('Проверка. Заголовок у сущности присутствует')
     def check_title_is_right(title):
         assert title != "", "Заголовок отсутствует"
 
     @staticmethod
-    @allure.step('Проверка. Количество сущностей > 2')
+    @allure.step('Проверка. Количество сущностей > 0')
     def check_count_of_entities(ids):
-        assert len(ids) > 0, "Количество сущностей меньше или равно 2"
+        assert ids > 0, "Количество сущностей меньше 0"
 
     @staticmethod
-    @allure.step('Проверка. id Созданного объекта вернулся')
+    @allure.step('Проверка. Код ответа положительный')
     def check_get_all_resp_num(code):
         assert code == 200, "Объект не создался"
 
@@ -46,7 +41,7 @@ class ChecksApi:
         assert code == 200, "Объект не выдался"
 
     @staticmethod
-    @allure.step('Проверка. Данные объекта обновлены')
+    @allure.step('Проверка. Данные объекта обновлены. Код ответа положительный')
     def check_entity_patched(code):
         assert code == 204, "Данные объекта не обновлены"
 
