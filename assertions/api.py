89ee37ffc255447e8174e57b94cc9a425b66c5f3
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 23:08:10 +0400

    Вместо checks_api.py стал просто api.py в директории assertions Множественные проверки заменены на 1 общую Убрана излишняя проверка

---
+++ assertions/api.py
@@ -0,0 +1,20 @@
+import allure
+
+
+class ChecksApi:
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    @staticmethod
+    @allure.step('Проверка. id Созданного объекта вернулся')
+    def check_create_resp_id(resp_id):
+        assert resp_id != 0, "Объект не создался"
+
+    @staticmethod
+    @allure.step('Проверка. Заголовок у сущности присутствует')
+    def check_title_is_right(title):
+        assert title != "", "Заголовок отсутствует"
+
+    @staticmethod
+    def assert_status_code(expected: int, actual: int):
+        assert expected == actual, f"Должен быть статус-код {expected}"
