5775463e51ffed091a44a58071aa10151dadbd91
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 22:57:28 +0400

    Добавлены проверки на статус-код для преждевременного отлавливания ошибки при вызове стандартных методов

--- api/requests/base_requests_api.py
+++ api/requests/base_requests_api.py
@@ -3,20 +3,38 @@ from requests import Response
 
 
 class BaseApi:
-
-
     @staticmethod
     def request_get(url: str) -> Response:
-        return requests.get(url)
+        req = requests.get(url)
+        if req.status_code == 200:
+            return req
+        else:
+            print(f"Ошибка! Получен код: {req.status_code}")
+            raise
 
     @staticmethod
     def request_post(url: str, json_req, **kwargs: dict) -> Response:
-        return requests.post(url, json=json_req, timeout=10, **kwargs)
+        req = requests.post(url, json=json_req, **kwargs)
+        if req.status_code == 200:
+            return req
+        else:
+            print(f"Ошибка! Получен код: {req.status_code}")
+            raise
 
     @staticmethod
     def request_delete(url: str, **kwargs: dict) -> Response:
-        return requests.delete(url, **kwargs)
+        req = requests.delete(url, **kwargs)
+        if req.status_code == 204:
+            return req
+        else:
+            print(f"Ошибка! Получен код: {req.status_code}")
+            raise
 
     @staticmethod
     def request_patch(url: str, json_req, **kwargs: dict) -> Response:
-        return requests.patch(url, json=json_req, timeout=10, **kwargs)
+        req = requests.patch(url, json=json_req, **kwargs)
+        if req.status_code == 204:
+            return req
+        else:
+            print(f"Ошибка! Получен код: {req.status_code}")
+            raise
