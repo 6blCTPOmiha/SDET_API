6c08eaa1251aa5df91a2b24dc34662e471aa33b3
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-10 12:31:55 +0400

    Крупное обновление - Удалена проверка кода ответа, тк перенесена в проверки - Добавлена функция удаления последнего элемента - Изменено: большинство функций возвращают статус код + необходимый ответ от сервера - Добавлена функция обновления данных последнего элемента - Добавлена функция очищения БД

--- helpers/api_helper.py
+++ helpers/api_helper.py
@@ -10,29 +10,6 @@ class ApiHelper(BaseApi):
     def __init__(self):
         super().__init__()
 
-    @allure.step("Проверка кода ответа")
-    def give_msg_response_code(self, response, obj='obj'):
-        if response.status_code == 200:
-            print(f'\nОперация прошла успешно!')
-        elif response.status_code == 201:
-            print(f'\nОбъект  успешно создан!')
-        elif response.status_code == 202:
-            if obj == 'dir':
-                print(f'\nНепустой каталог  поставлен в очередь на удаление!')
-        elif response.status_code == 204:
-            if obj == 'obj':
-                print(f'\nОбъект  успешно удалён!')
-        elif response.status_code == 404:
-            print(f'\nОбъект  не найден!')
-        elif response.status_code == 405:
-            print(f'\nМетод не поддерживается!')
-        elif response.status_code == 409:
-            print(f'\nПо пути уже существует объект с таким же...')
-        else:
-            json_response = response.json()
-            print(response, json_response)
-
-
     def create_model_request(self):
         url = URL_CREATE
         payload = EMREQ0.model_dump()
@@ -47,6 +24,13 @@ class ApiHelper(BaseApi):
         response_ch = ConvertHelper.deserialize_response(response_text)
         return response_ch.title
 
+    def get_msg_response_by_last_id(self):
+        ids = self.get_ids_msgs_response()
+        url_id = f'{URL_GET}/{ids[-1]}'
+        response = self.request_get(url_id)
+        response_text = response.text
+        response_ch = ConvertHelper.deserialize_response(response_text)
+        return response.status_code, response_ch.title
 
     def get_ids_msgs_response(self):
         url = URL_GET_ALL
@@ -59,13 +43,10 @@ class ApiHelper(BaseApi):
 
     def get_msgs_response(self):
         url = URL_GET_ALL
+        response = self.request_get(url)
         response_text = self.request_get(url).text
         response_chs = ConvertHelper.deserialize_responses(response_text)
-        """
-        for ch in response_chs:
-            print(f'\n{ch}\n')
-        """
-        return response_chs
+        return response.status_code, response_chs
 
     def delete_by_id(self, element_id: int):
         url_id = f'{URL_DELETE}/{element_id}'
@@ -86,3 +67,17 @@ class ApiHelper(BaseApi):
         response = self.request_patch(url, json_req=payload)
         response.raise_for_status()
         return response
+
+    def patch_by_last_id(self):
+        ids = self.get_ids_msgs_response()
+        url = f'{URL_PATCH}/{ids[-1]}'
+        payload = EMREQ0.model_dump()
+        response = self.request_patch(url, json_req=payload)
+        response.raise_for_status()
+        return response.status_code
+
+    def clear_db(self):
+        ids = self.get_ids_msgs_response()
+        for ent_id in ids:
+            self.delete_by_id(ent_id)
+        return 'OK'
