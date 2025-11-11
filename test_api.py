0d958745b0ccdcd86346786ce4bc8d246f560fff
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-11 11:51:52 +0400

    Удалены излишние тесты. Айдишник теперь получается благодаря фикстуре. Названия тестов скорректированы

--- test_api.py
+++ test_api.py
@@ -7,60 +7,34 @@ from data.data_objs import EMREQ0
 @allure.feature('Тестирование работы api сервиса')
 class TestCustomAPI:
     @allure.title('Проверка создания сущности')
-    @pytest.mark.no_custom_id
     @pytest.mark.create
-    def test_create(self, ent_api):
-        resp = ent_api.create_model_request(EMREQ0)
+    def test_create_entity(self, ent_api):
+        resp = ent_api[0].create_model_request(EMREQ0)
+        status_code, title = ent_api[0].get_msg_response_by_id(int(resp.text))
         AssertionsApi.assert_status_code(200, resp.status_code)
-        AssertionsApi.check_create_resp_id(resp.text)
-
-    @allure.title('Проверка удаления последней сущности')
-    @pytest.mark.no_custom_id
-    @pytest.mark.delete
-    def test_delete_last(self, ent_api):
-        resp = ent_api.delete_last_entity()
-        AssertionsApi.assert_status_code(204, resp.status_code)
+        AssertionsApi.check_title_exists(title)
 
     @allure.title('Проверка удаления сущности по id')
-    @pytest.mark.custom_id
     @pytest.mark.delete
-    def test_delete_by_id(self, ent_api):
-        resp = ent_api.delete_by_id(284)
+    def test_delete_entity(self, ent_api):
+        resp = ent_api[0].delete_by_id(ent_api[1])
         AssertionsApi.assert_status_code(204, resp.status_code)
 
-    @allure.title('Проверка получения последней сущности')
-    @pytest.mark.no_custom_id
-    @pytest.mark.get
-    def test_get_last(self, ent_api):
-        status_code, title = ent_api.get_msg_response_by_last_id()
-        AssertionsApi.assert_status_code(200, status_code)
-        AssertionsApi.check_title_is_right(title)
-
     @allure.title('Проверка получения сущности по id')
-    @pytest.mark.custom_id
     @pytest.mark.get
-    def test_get_by_id(self, ent_api):
-        status_code, title = ent_api.get_msg_response_by_id(286)
+    def test_get_entity(self, ent_api):
+        status_code, title = ent_api[0].get_msg_response_by_id(ent_api[1])
         AssertionsApi.assert_status_code(200, status_code)
-        AssertionsApi.check_title_is_right(title)
+        AssertionsApi.check_title_exists(title)
 
     @allure.title('Проверка получения всех сущностей')
-    @pytest.mark.no_custom_id
     @pytest.mark.get_all
-    def test_get_all(self, ent_api):
-        status_code, full = ent_api.get_msgs_response()
+    def test_get_all_entities(self, ent_api):
+        status_code, full = ent_api[0].get_msgs_response()
         AssertionsApi.assert_status_code(200, status_code)
 
-    @allure.title('Проверка изменения параметров последней сущности')
-    @pytest.mark.no_custom_id
-    @pytest.mark.patch
-    def test_patch(self, ent_api):
-        status_code = ent_api.patch_by_last_id(EMREQ0)
-        AssertionsApi.assert_status_code(204, status_code)
-
     @allure.title('Проверка изменения параметров сущности по id')
-    @pytest.mark.custom_id
     @pytest.mark.patch
-    def test_patch_by_id(self, ent_api):
-        status_code = ent_api.patch_by_id(EMREQ0, 289)
+    def test_patch_entity(self, ent_api):
+        status_code = ent_api[0].patch_by_id(EMREQ0, ent_api[1])
         AssertionsApi.assert_status_code(204, status_code)
