b0f21b6a719a6c5004f20d9ef4704921bdc2c1d7
6blCTPOmiha <bystrovmiha@gmail.com>
2025-11-11 11:50:41 +0400

    Добавлена отправка айдишника в тесты и улучшена очистка после прохождения теста

--- conftest.py
+++ conftest.py
@@ -1,14 +1,17 @@
 import pytest
-from helpers.entity_api import EntityApi
+from api.entity_api import EntityApi
 from data.data_objs import EMREQ0
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def ent_api():
     ent_api = EntityApi()
-    ent_api.create_model_request(EMREQ0)
-    yield ent_api
-    ent_api.delete_last_entity()
+    ent = ent_api.create_model_request(EMREQ0)
+    ent_id = int(ent.text)
+    yield ent_api, ent_id
+    ids = ent_api.get_ids_msgs_response()
+    if ent_id in ids:
+        ent_api.delete_by_id(element_id=ent_id)
 
 
 @pytest.fixture(scope="class", autouse=True)
